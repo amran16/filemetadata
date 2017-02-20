a0a45bd1fb9a6f25cd81dc0a0c33828579835b94
amran16 <maryamg99@gmail.com>
2017-02-19 21:45:03 -0800

    initial upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var multer  = require('multer');
+
+var upload = multer({ dest: 'uploads/' });
+
+var app = express();
+
+//Set static path
+app.use(express.static(__dirname + '/public'));
+
+//view engine
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.post('/addfile', multer().single('inputfile'), function (req, res, next) {
+
+    var files = req.file;
+
+    var data ={
+        name: files.originalname,
+        size: files.size + ' bytes'
+    }
+
+    res.send(data);
+    console.log('here is the file:', files);
+
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+      console.log("Server running on port 3000");
+});
